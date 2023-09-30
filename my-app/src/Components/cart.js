6922ba414dc71a3092db6a6b8c5f5cc35390aeeb
Veena Rajan <144211827+veenarajan1109@users.noreply.github.com>
2023-09-30 21:38:46 +0530

    Update cart.js

--- my-app/src/Components/cart.js
+++ my-app/src/Components/cart.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
 const Cart = ({ cartItems }) => {
-  console.log("cartItems:", cartItems);
+  
 
   return (
     <div className='cartitems'>
       <div className='cartitems-header'>
         Items Added To Cart:
       </div>
-      {cartItems && cartItems.length === 0 ? (
-        <div className='cartitem-empty'>No Items added to cart</div>
-      ) : (
-        <div>
-        {cartItems && cartItems.length > 0 && cartItems.map((item, index) => (
-            <div key={item.id} className='cartitem-list'>
-              <img
-                className='cartitems-image'
-                src={item.Image}
-                alt={item.Name}
-              />
-            </div>
-          ))}
-        </div>
+      {cartItems.length === 0 &&(
+        <div className='cartitems-empty'>No Items are Added.</div>
       )}
+
+      <div>
+        {cartItems.map((item)=>(
+          <div key={item.id} className='cartitem-list'>
+            <img
+            className='cartitems-image'
+            src={item.image}
+            alt={item.name}
+            />
+            <div className='cartitems-name'>{item.name}</div>
+          </div>
+        ))}
+      </div>
     </div>
+    
   );
 };
 
 export default Cart;
+
+        
